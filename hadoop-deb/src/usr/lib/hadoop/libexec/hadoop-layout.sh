3de47f484dad1b05ba324500a46004b8752ef567
Atkins <atkinschang@icloud.com>
2014-05-02 02:16:22 +0800

    fix path and scripts

---
+++ hadoop-deb/src/usr/lib/hadoop/libexec/hadoop-layout.sh
@@ -0,0 +1,11 @@
+HADOOP_CONF_DIR=/etc/hadoop
+HADOOP_LOG_DIR=/var/log/hadoop/hdfs
+HADOOP_PID_DIR=/var/run/hadoop/hdfs
+
+HADOOP_MAPRED_LOG_DIR=/var/log/hadoop/mapred
+HADOOP_MAPRED_PID_DIR=/var/run/hadoop/mapred
+
+
+YARN_CONF_DIR=/etc/hadoop
+YARN_LOG_DIR=/var/log/hadoop/yarn
+YARN_PID_DIR=/var/run/hadoop/yarn
